5522c2c074e5c06cfcc1e02ff08b9b9613003478
Veluga <d.vela1@hotmail.com>
2022-02-04 01:23:55 -0600

    Added function extract_from_url to the fetch module
    I programmed it to include a progress bar.
    Used this new extract_from_url function in the download function fetch.chexpert()

--- src/download/fetch.py
+++ src/download/fetch.py
@@ -9,6 +9,8 @@ import zipfile
 import io
 from os import rename
 from os import remove
+from tqdm import tqdm
+
 
 
 
@@ -20,18 +22,41 @@ raw_data_path = '../data/raw/'
 
 #print("fetch called from \n"+os.getcwd())
 
-def extract_from_url(url,path):
+
+
+# Streaming, so we can iterate over the response.
+
+
+
+
+def extract_from_url(url=' https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-large-zip-file.zip',path='here'):
     '''
-    This function takes a zip download url and extracts it to the provided path
+    This function takes a zip download url and extracts it to the given path. 
+    
+    The function also graphs a progress bar that indicates download speeds. 
 
     '''
-    try:
-        r = requests.get(url)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall(raw_data_path+url)
-        print('done')
-    except:
-        print('errors downloading')
+    zip_path=path+'.zip'
+    response = requests.get(url, stream=True)
+    total_size_in_bytes= int(response.headers.get('content-length', 0))
+    block_size = 1024 #1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(zip_path, 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
+        
+    print('Extracting from zip')
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(path)
+    remove(zip_path)
+        
+        
+        
+
     
 def medpix():
     '''
@@ -62,20 +87,14 @@ def chexpert():
     from the url provided to us when we made stanford signin.
     '''
     print('downloading chexpert')
-    try:
-        chexpert_url = ('https://us13.mailchimp.com/mctx/clicks?url=http%3A%2F%2Fdownload'
-                        '.cs.stanford.edu%2Fdeep%2FCheXpert-v1.0-small.zip&h=7af997182d92'
-                        '98f2fdbc10da45f4f70341158f140e891ea3e6ef48d0e7d65646&v=1&xid=eed9'
-                        '8b1fbb&uid=55365305&pool=contact_facing&subject=CheXpert-v1.0%3A+S'
-                        'ubscription+Confirmed')
+    chexpert_url = ('https://us13.mailchimp.com/mctx/clicks?url=http%3A%2F%2Fdownload'
+                    '.cs.stanford.edu%2Fdeep%2FCheXpert-v1.0-small.zip&h=7af997182d92'
+                    '98f2fdbc10da45f4f70341158f140e891ea3e6ef48d0e7d65646&v=1&xid=eed9'
+                    '8b1fbb&uid=55365305&pool=contact_facing&subject=CheXpert-v1.0%3A+S'
+                    'ubscription+Confirmed')
+    extract_from_url(chexpert_url,raw_data_path +'chexpert')
 # =============================================================================
 #         chexpert_url ='https://sample-videos.com/zip/20mb.zip'
 # =============================================================================
 
-        r = requests.get(chexpert_url)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall(raw_data_path+'chexpert')
-        print('done')
-    except:
-        print('errors downloading chexpert')
 
