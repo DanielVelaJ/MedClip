e5fa6aba18412671b2cb1e59d55eb1e35336d2ce
Veluga <d.vela1@hotmail.com>
2022-02-04 00:20:10 -0600

    updated download_all.py to include a line generating data/raw directory

--- src/download_all.py
+++ src/download_all.py
@@ -5,6 +5,12 @@ Created on Thu Feb  3 15:57:19 2022
 @author: danic
 """
 import download.fetch as ft
+import os
+
+path='../data/raw'
+if not os.path.exists(path):
+    os.makedirs()
+    print('created data/raw folders')
 
 
 ft.chexpert()
